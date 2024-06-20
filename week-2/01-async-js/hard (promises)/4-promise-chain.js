09a915f7d4f46cb98fa53bc619838f701a1d2dcd
Puneet Uttam <puneet.uttam.99@gmail.com>
2024-06-20 07:05:50 +0530

    Sol

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,42 @@
  */
 
 function wait1(t) {
-
+  //return new Promise(function(resolve, reject){})
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, t * 1000);
+  });
 }
 
 function wait2(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, t * 1000);
+  });
 }
 
 function wait3(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, t * 1000);
+  });
 }
 
 function calculateTime(t1, t2, t3) {
-
+  const startTime = new Date().getTime();
+  return wait1(t1)
+    .then((value) => {
+      return wait2(t2);
+    })
+    .then((value) => {
+      return wait3(t3);
+    })
+    .then((value) => {
+      return new Date().getTime() - startTime;
+    });
 }
 
 module.exports = calculateTime;
