05d73222be580bb77404632a0d610d7248f0572a
Puneet Uttam <puneet.uttam.99@gmail.com>
2024-06-26 07:40:06 +0530

    Updates

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,29 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb url');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    pasword:String
+    
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    password:String
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title:String,
+    description:String,
+    price:String,
+    imageLink:String
+
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
